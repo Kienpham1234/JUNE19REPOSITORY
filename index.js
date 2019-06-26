8fae5fd97e8a2ea09b6723975afb76911c83328a
Kien Pham <747782@student.tor.loc>
2019-06-26 13:37:10 -0400

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello');
+
+
+//factory function
+function createCircle(radius){
+return{
+    radius,
+    draw: function(){
+        console.log('draw');
+    }
+};
+}
+const circle = createCircle(1);
+
+// Constructor function
+const Circle1 = new Function('radius',`
+this.radius = radius;
+this.draw = function(){
+        console.log('draw');
+}
+`);
+const orange = new Circle1(1);
+const another = new Circle1(1);
